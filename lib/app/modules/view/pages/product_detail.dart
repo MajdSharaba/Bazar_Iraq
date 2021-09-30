0072f35c6589f58be3c50f98462dae133cebb7d4
Majd Sharabah <MajdSharabah8396@gmail.com>
2021-09-30 13:35:11 +0300

    majd commit3

---
+++ lib/app/modules/view/pages/product_detail.dart
@@ -0,0 +1,390 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:pazar_iraq/app/core/constants.dart';
+import 'package:pazar_iraq/app/core/light_color.dart';
+import 'package:pazar_iraq/app/model/product.dart';
+import 'package:pazar_iraq/app/modules/view/widgets/title_text.dart';
+
+
+
+
+///image for CarouselSlider
+final List<String> exampleImage = [
+  'assets/shoe_tilt_2.png',
+  'assets/shoe_tilt_2.png',
+  'assets/shoe_tilt_2.png',
+  'assets/shoe_tilt_2.png',
+  'assets/shoe_tilt_2.png'
+];
+
+
+class ProductDetailPage extends StatefulWidget {
+  Product? product;
+  ProductDetailPage({Key? key,this.product}) : super(key: key);
+
+  @override
+  _ProductDetailPageState createState() => _ProductDetailPageState();
+}
+
+class _ProductDetailPageState extends State<ProductDetailPage>
+    with TickerProviderStateMixin {
+  AnimationController? controller;
+  Animation<double>? animation;
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    animation = Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(parent: controller!, curve: Curves.easeInToLinear));
+    controller!.forward();
+  }
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+
+  bool isLiked = true;
+  Widget _appBar() {
+    return Container(
+      padding: padding,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          BackButton(),
+          _icon(isLiked ? Icons.favorite : Icons.favorite_border,
+              color: isLiked ? LightColor.red : LightColor.lightGrey,
+              size: 15,
+              padding: 12,
+              isOutLine: false, onPressed: () {
+            setState(() {
+              isLiked = !isLiked;
+            });
+          }),
+        ],
+      ),
+    );
+  }
+///// favorite icon
+  Widget _icon(
+    IconData icon, {
+    Color color = LightColor.iconColor,
+    double size = 20,
+    double padding = 10,
+    bool isOutLine = false,
+    Function? onPressed,
+  }) {
+    return InkWell(
+      child: Container(
+        height: 40,
+        width: 40,
+        padding: EdgeInsets.all(padding),
+        // margin: EdgeInsets.all(padding),
+        decoration: BoxDecoration(
+          border: Border.all(
+              color: LightColor.iconColor,
+              style: isOutLine ? BorderStyle.solid : BorderStyle.none),
+          borderRadius: BorderRadius.all(Radius.circular(13)),
+          color:
+              isOutLine ? Colors.transparent : Theme.of(context).backgroundColor,
+          boxShadow: const <BoxShadow>[
+            BoxShadow(
+                color: Color(0xfff8f8f8),
+                blurRadius: 5,
+                spreadRadius: 10,
+                offset: Offset(5, 5)),
+          ],
+        ),
+        child: Icon(icon, color: color, size: size),
+      ),
+      onTap: (){if
+      (onPressed != null) {
+        onPressed();
+      }},
+    );
+  }
+//// productImage
+  Widget _productImage() {
+    return AnimatedBuilder(
+      builder: (context, child) {
+        return AnimatedOpacity(
+          duration: const Duration(milliseconds: 500),
+          opacity: animation!.value,
+          child: child,
+        );
+      },
+      animation: animation!,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+
+     CarouselSlider(
+    options: CarouselOptions(
+    aspectRatio: 2.0,
+    enlargeCenterPage: true,
+    autoPlay: false,
+    ),
+    items: CarouselSliderDetails,
+    ),
+
+          // Image.asset(widget.product!.image!)
+        ],
+      ),
+    );
+  }
+/////detailwidget
+  Widget _detailWidget() {
+    return DraggableScrollableSheet(
+      maxChildSize: .8,
+      initialChildSize: .53,
+      minChildSize: .53,
+      builder: (context, scrollController) {
+        return Container(
+          padding: padding.copyWith(bottom: 0),
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40),
+                topRight: Radius.circular(40),
+              ),
+              color: Colors.white),
+          child: SingleChildScrollView(
+            controller: scrollController,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                const SizedBox(height: 5),
+                Container(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: 50,
+                    height: 5,
+                    decoration: const BoxDecoration(
+                        color: LightColor.iconColor,
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    TitleText(text: widget.product!.name, fontSize: 25),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            const TitleText(
+                              text: "\$ ",
+                              fontSize: 18,
+                              color: LightColor.red,
+                            ),
+                            TitleText(
+                              text:  widget.product!.price.toString(),
+                              fontSize: 25,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: const <Widget>[
+                            Icon(Icons.star,
+                                color: LightColor.yellowColor, size: 17),
+                            Icon(Icons.star,
+                                color: LightColor.yellowColor, size: 17),
+                            Icon(Icons.star,
+                                color: LightColor.yellowColor, size: 17),
+                            Icon(Icons.star,
+                                color: LightColor.yellowColor, size: 17),
+                            Icon(Icons.star_border, size: 17),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                _availableSize(),
+                const SizedBox(
+                  height: 20,
+                ),
+
+
+
+
+
+                const SizedBox(
+                  height: 20,
+                ),
+                _availableColor(),
+
+                const SizedBox(
+                  height: 20,
+                ),
+                _description(),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+////// widget for availableSize size
+  Widget _availableSize() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const TitleText(
+          text: "Available Size",
+          fontSize: 14,
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _sizeWidget("US 6"),
+            _sizeWidget("US 7", isSelected: true),
+            _sizeWidget("US 8"),
+            _sizeWidget("US 9"),
+          ],
+        )
+      ],
+    );
+  }
+///// widget for size design
+  Widget _sizeWidget(String text, {bool isSelected = false}) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(
+            color: LightColor.iconColor,
+            style: !isSelected ? BorderStyle.solid : BorderStyle.none),
+        borderRadius: BorderRadius.all(const Radius.circular(13)),
+        color:
+            isSelected ? LightColor.orange : Theme.of(context).backgroundColor,
+      ),
+      child: TitleText(
+        text: text,
+        fontSize: 16,
+        color: isSelected ? LightColor.background : LightColor.titleTextColor,
+      ),
+    );
+  }
+///// widget for color available
+  Widget _availableColor() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const TitleText(
+          text: "Available Size",
+          fontSize: 14,
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            _colorWidget(LightColor.yellowColor, isSelected: true),
+            const SizedBox(
+              width: 30,
+            ),
+            _colorWidget(LightColor.lightBlue),
+            const SizedBox(
+              width: 30,
+            ),
+            _colorWidget(LightColor.black),
+            const SizedBox(
+              width: 30,
+            ),
+            _colorWidget(LightColor.red),
+            const SizedBox(
+              width: 30,
+            ),
+            _colorWidget(LightColor.skyBlue),
+          ],
+        )
+      ],
+    );
+  }
+
+//// widget for clor design
+  Widget _colorWidget(Color color, {bool isSelected = false}) {
+    return CircleAvatar(
+      radius: 12,
+      backgroundColor: color.withAlpha(150),
+      child: isSelected
+          ? Icon(
+              Icons.check_circle,
+              color: color,
+              size: 18,
+            )
+          : CircleAvatar(radius: 7, backgroundColor: color),
+    );
+  }
+
+  //// description widget
+  Widget _description() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const TitleText(
+          text: "Description",
+          fontSize: 14,
+        ),
+        const SizedBox(height: 20),
+        Text( widget.product!.category.toString()),
+      ],
+    );
+  }
+  //// widget for CarouselSliderDetails
+  final List<Widget> CarouselSliderDetails = exampleImage
+      .map((item) => Container(
+        margin: const EdgeInsets.all(5.0),
+        child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+            child: Stack(
+              children: <Widget>[
+                Image.asset(item, fit: BoxFit.cover, width: 1000.0),
+              ],
+            )),
+      ))
+      .toList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color(0xfffbfbfb),
+              Color(0xfff7f7f7),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          )),
+          child: Stack(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  _appBar(),
+                  _productImage(),
+                  // _categoryWidget(),
+                ],
+              ),
+              _detailWidget()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
